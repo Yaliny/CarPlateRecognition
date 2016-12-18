472c5fb2edf672087ea3532ff6712fe133d042ac
mmatejko <m.matejko@gmail.com>
2016-12-18 19:01:25 +0100

    refactoring + added images output (todo)

--- program.m
+++ program.m
@@ -28,11 +28,11 @@ function varargout = program(varargin)
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
-                   'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @program_OpeningFcn, ...
-                   'gui_OutputFcn',  @program_OutputFcn, ...
-                   'gui_LayoutFcn',  [] , ...
-                   'gui_Callback',   []);
+    'gui_Singleton',  gui_Singleton, ...
+    'gui_OpeningFcn', @program_OpeningFcn, ...
+    'gui_OutputFcn',  @program_OutputFcn, ...
+    'gui_LayoutFcn',  [] , ...
+    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
@@ -43,74 +43,157 @@ else
     gui_mainfcn(gui_State, varargin{:});
 end
 % End initialization code - DO NOT EDIT
-
+end
 
 % --- Executes just before program is made visible.
 function program_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to program (see VARARGIN)
-
-% Choose default command line output for program
 handles.output = hObject;
-
-% Update handles structure
 guidata(hObject, handles);
-
-% UIWAIT makes program wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
+end
 
 % --- Outputs from this function are returned to the command line.
-function varargout = program_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
+function varargout = program_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
-
+end
 
 % --- Executes on button press in fileDialog.
 function fileDialog_Callback(hObject, eventdata, handles)
-% hObject    handle to fileDialog (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 [fileName,pathName] = uigetfile({'*.mp4'; '*.mkv'; '*.avi'},'Select the video');
 set(handles.filenameText, 'String', fileName);
 setappdata(0, 'file_name', fileName);
 setappdata(0, 'path_name', pathName);
-videoSrc = vision.VideoFileReader(strcat(pathName, fileName))
-
+videoSrc = vision.VideoFileReader(strcat(pathName, fileName));
+[hFig, hAxes] = createFigureAndAxes();
+insertButtons(hFig, hAxes, videoSrc);
+end
 
 % --- Executes on button press in proceedButton.
 function proceedButton_Callback(hObject, eventdata, handles)
-% hObject    handle to proceedButton (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
 x = getappdata(0, 'file_name')
 y = getappdata(0, 'path_name')
+end
 
 function log_Callback(hObject, eventdata, handles)
-% hObject    handle to log (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of log as text
-%        str2double(get(hObject,'String')) returns contents of log as a double
-
+end
 
 % --- Executes during object creation, after setting all properties.
 function log_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to log (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
+end
+end
+
+function [hFig, hAxes] = createFigureAndAxes()
+
+% Close figure opened by last run
+figTag = 'CVST_VideoOnAxis_9804532';
+close(findobj('tag',figTag));
+
+% Create new figure
+hFig = figure('numbertitle', 'off', ...
+    'name', 'Video In Custom GUI', ...
+    'menubar','none', ...
+    'toolbar','none', ...
+    'resize', 'on', ...
+    'tag',figTag, ...
+    'renderer','painters', ...
+    'position',[680 678 480 240]);
+
+% Create axes and titles
+hAxes.axis1 = createPanelAxisTitle(hFig,[0.1 0.2 0.36 0.6],'Original Video'); % [X Y W H]
+hAxes.axis2 = createPanelAxisTitle(hFig,[0.5 0.2 0.36 0.6],'Rotated Video');
+end
+
+function hAxis = createPanelAxisTitle(hFig, pos, axisTitle)
+
+% Create panel
+hPanel = uipanel('parent',hFig,'Position',pos,'Units','Normalized');
+
+% Create axis
+hAxis = axes('position',[0 0 1 1],'Parent',hPanel);
+hAxis.XTick = [];
+hAxis.YTick = [];
+hAxis.XColor = [1 1 1];
+hAxis.YColor = [1 1 1];
+titlePos = [pos(1)+0.02 pos(2)+pos(3)+0.3 0.3 0.07];
+uicontrol('style','text',...
+    'String', axisTitle,...
+    'Units','Normalized',...
+    'Parent',hFig,'Position', titlePos,...
+    'BackgroundColor',hFig.Color);
+end
+
+function insertButtons(hFig,hAxes,videoSrc)
+
+% Play button with text Start/Pause/Continue
+uicontrol(hFig,'unit','pixel','style','pushbutton','string','Start',...
+    'position',[10 10 75 25], 'tag','PBButton123','callback',...
+    {@playCallback,videoSrc,hAxes});
+
+% Exit button with text Exit
+uicontrol(hFig,'unit','pixel','style','pushbutton','string','Exit',...
+    'position',[100 10 50 25],'callback', ...
+    {@exitCallback,videoSrc,hFig});
+end
+
+function playCallback(hObject,~,videoSrc,hAxes)
+try
+    % Check the status of play button
+    isTextStart = strcmp(hObject.String,'Start');
+    isTextCont  = strcmp(hObject.String,'Continue');
+    if isTextStart
+        % Two cases: (1) starting first time, or (2) restarting
+        % Start from first frame
+        if isDone(videoSrc)
+            reset(videoSrc);
+        end
+    end
+    if (isTextStart || isTextCont)
+        hObject.String = 'Pause';
+    else
+        hObject.String = 'Continue';
+    end
+    
+    % Rotate input video frame and display original and rotated
+    % frames on figure
+    angle = 0;
+    while strcmp(hObject.String, 'Pause') && ~isDone(videoSrc)
+        % Get input video frame and rotated frame
+        [frame,rotatedImg,angle] = getAndProcessFrame(videoSrc,angle);
+        % Display input video frame on axis
+        showFrameOnAxis(hAxes.axis1, frame);
+        % Display rotated video frame on axis
+        showFrameOnAxis(hAxes.axis2, rotatedImg);
+    end
+    
+    % When video reaches the end of file, display "Start" on the
+    % play button.
+    if isDone(videoSrc)
+        hObject.String = 'Start';
+    end
+catch ME
+    % Re-throw error message if it is not related to invalid handle
+    if ~strcmp(ME.identifier, 'MATLAB:class:InvalidHandle')
+        rethrow(ME);
+    end
+end
+end
+
+function [frame,rotatedImg,angle] = getAndProcessFrame(videoSrc,angle)
+
+% Read input video frame
+frame = step(videoSrc);
+
+% Pad and rotate input video frame
+paddedFrame = padarray(frame, [30 30], 0, 'both');
+rotatedImg  = imrotate(paddedFrame, angle, 'bilinear', 'crop');
+angle       = angle + 1;
+end
+
+function exitCallback(~,~,videoSrc,hFig)
+
+% Close the video file
+release(videoSrc);
+% Close the figure window
+close(hFig);
 end
