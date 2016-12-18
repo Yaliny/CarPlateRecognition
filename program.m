b51aed6466659970ef84aa77cd3ac06883602a94
mmatejko <m.matejko@gmail.com>
2016-12-18 14:59:02 +0100

    gui init

---
+++ program.m
@@ -0,0 +1,116 @@
+
+function varargout = program(varargin)
+% PROGRAM MATLAB code for program.fig
+%      PROGRAM, by itself, creates a new PROGRAM or raises the existing
+%      singleton*.
+%
+%      H = PROGRAM returns the handle to a new PROGRAM or the handle to
+%      the existing singleton*.
+%
+%      PROGRAM('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PROGRAM.M with the given input arguments.
+%
+%      PROGRAM('Property','Value',...) creates a new PROGRAM or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before program_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to program_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help program
+
+% Last Modified by GUIDE v2.5 18-Dec-2016 14:03:43
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @program_OpeningFcn, ...
+                   'gui_OutputFcn',  @program_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before program is made visible.
+function program_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to program (see VARARGIN)
+
+% Choose default command line output for program
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes program wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = program_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in fileDialog.
+function fileDialog_Callback(hObject, eventdata, handles)
+% hObject    handle to fileDialog (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[fileName,pathName] = uigetfile({'*.mp4'; '*.mkv'; '*.avi'},'Select the video');
+set(handles.filenameText, 'String', fileName);
+setappdata(0, 'file_name', fileName);
+setappdata(0, 'path_name', pathName);
+videoSrc = vision.VideoFileReader(strcat(pathName, fileName))
+
+
+% --- Executes on button press in proceedButton.
+function proceedButton_Callback(hObject, eventdata, handles)
+% hObject    handle to proceedButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+x = getappdata(0, 'file_name')
+y = getappdata(0, 'path_name')
+
+function log_Callback(hObject, eventdata, handles)
+% hObject    handle to log (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of log as text
+%        str2double(get(hObject,'String')) returns contents of log as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function log_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to log (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
